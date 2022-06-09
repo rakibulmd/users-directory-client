9c640e871766e92e4c6bb2c8fffd59e947d1c070
Rakibul md <rakib245875@gmail.com>
2022-06-09 16:08:20 +0600

    Added: Header design updated, all user headline added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,8 +3,11 @@ import Header from "../Shared/Header";
 
 const Home = () => {
     return (
-        <div className="bg-gradient-to-r from-primary to-secondary min-h-screen">
+        <div className="bg-gradient-to-r from-primary to-secondary min-h-screen text-white">
             <Header></Header>
+            <div className="text-center text-3xl md:text-5xl mt-12">
+                <h2>All Users</h2>
+            </div>
         </div>
     );
 };
