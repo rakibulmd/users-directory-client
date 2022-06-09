e94b64367b08433967badef10c20aeeb9405fc64
Rakibul md <rakib245875@gmail.com>
2022-06-10 01:58:20 +0600

    Updated: Table data updated and active filter icon visibility implemented

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,41 +3,35 @@ import React, { useEffect, useState } from "react";
 import Header from "../Shared/Header";
 import TableRows from "./TableRows";
 import { FaCaretDown, FaCaretUp } from "react-icons/fa";
-import { IconContext } from "react-icons";
 
 const Home = () => {
     const [users, setUsers] = useState([]);
-    const [message, setMessage] = useState("");
+
+    const [activeFilter, setActiveFilter] = useState(null);
     const handleNameAscending = (event) => {
         event.preventDefault();
         const newUsers = users.sort((a, b) =>
-            a.email.toLowerCase() > b.email.toLowerCase() ? 1 : -1
+            a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
         );
-        console.log(newUsers);
         setUsers(newUsers);
-        console.log(users);
-        setMessage("up");
+        setActiveFilter("nameAscending");
     };
     const handleNameDescending = (event) => {
         event.preventDefault();
         const newUsers = users.sort((a, b) =>
-            a.email.toLowerCase() < b.email.toLowerCase() ? 1 : -1
+            a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1
         );
-        console.log(newUsers);
         setUsers(newUsers);
-        console.log(users);
-        setMessage("down");
+        setActiveFilter("nameDescending");
     };
     useEffect(() => {
         const getData = async () => {
-            const response = await axios.get(
-                `https://assessment-project-server.herokuapp.com/customers`
-            );
-
-            setUsers(response.data);
+            await axios
+                .get(`http://localhost:5000/customers`)
+                .then((response) => setUsers(response.data))
+                .catch((error) => console.log(error));
         };
         getData();
-        console.log("data loaded");
     }, []);
 
     return (
@@ -45,7 +39,6 @@ const Home = () => {
             <Header></Header>
             <div className="text-center text-3xl md:text-5xl mt-12">
                 <h2>All Users</h2>
-                {/* <h2>{message}</h2> */}
             </div>
             <div class="overflow-x-auto p-5 text-black">
                 <table class="table table-zebra w-full">
@@ -60,19 +53,31 @@ const Home = () => {
                                     <span className="nameUp">
                                         <FaCaretUp
                                             onClick={handleNameAscending}
-                                            className="text-gray-400 w-4 h-4 inline"
+                                            className={`${
+                                                activeFilter === "nameAscending"
+                                                    ? "text-gray-900 w-4 h-4 inline"
+                                                    : "text-gray-400 w-4 h-4 inline"
+                                            }`}
                                         />
                                     </span>
                                     <span className="nameDown">
                                         <FaCaretDown
                                             onClick={handleNameDescending}
-                                            className="text-gray-400 w-4 h-4 inline"
+                                            className={`${
+                                                activeFilter ===
+                                                "nameDescending"
+                                                    ? "text-gray-900 w-4 h-4 inline"
+                                                    : "text-gray-400 w-4 h-4 inline"
+                                            }`}
                                         />
                                     </span>
                                 </span>
                             </th>
-                            <th>Email</th>
-                            <th>Favorite Color</th>
+                            <th>Position</th>
+                            <th>State</th>
+                            <th>Age</th>
+                            <th>Start Date</th>
+                            <th>Salary</th>
                         </tr>
                     </thead>
                     <tbody>
--- src/Pages/Home/TableRows.js
+++ src/Pages/Home/TableRows.js
@@ -3,9 +3,12 @@ import React from "react";
 const TableRows = ({ user }) => {
     return (
         <tr>
-            <td>{user.profile.name}</td>
-            <td>{user.email}</td>
-            <td>Blue</td>
+            <td>{user?.name}</td>
+            <td>{user?.position}</td>
+            <td>{user?.address}</td>
+            <td>{user?.age}</td>
+            <td>{user?.startDate}</td>
+            <td>{user?.salary}</td>
         </tr>
     );
 };
